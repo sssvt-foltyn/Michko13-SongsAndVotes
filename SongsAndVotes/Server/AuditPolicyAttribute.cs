7edffd63b5c9acecf9460b0e4df0af40f7c198a3
Pavel Foltyn <pavel.foltyn@volny.cz>
2021-04-10 21:25:11 +0200

    TECH Test the integrated middleware.

---
+++ SongsAndVotes/Server/AuditPolicyAttribute.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+
+namespace SongsAndVotes.Server
+{
+
+
+
+    public class AuditPolicyAttribute
+    {
+
+
+
+        public bool NeedsAudit { get; }
+
+
+
+        public AuditPolicyAttribute(bool needsAudit)
+        {
+            NeedsAudit = needsAudit;
+        }
+
+
+
+    }
+
+
+
+}
--- SongsAndVotes/Server/Program.cs
+++ SongsAndVotes/Server/Program.cs
@@ -20,7 +20,8 @@ namespace SongsAndVotes.Server
 			Host.CreateDefaultBuilder(args)
 				.ConfigureWebHostDefaults(webBuilder =>
 				{
-					webBuilder.UseStartup<Startup>();
+					//webBuilder.UseStartup<Startup>();
+					webBuilder.UseStartup<IntegratedMiddlewareStartup>();
 				});
 	}
 }
