b5e77f0436af71ef7f06f45205815bdbdd5f2b21
Pavel Foltyn <pavel.foltyn@volny.cz>
2021-04-15 13:35:23 +0200

    TECH Add a db schema generation script. Switch from localdb to my local SQL Server instance.

--- SongsAndVotes/Server/Startup.cs
+++ SongsAndVotes/Server/Startup.cs
@@ -23,7 +23,8 @@ namespace SongsAndVotes.Server
 		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
 		public void ConfigureServices(IServiceCollection services)
 		{
-			services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+			//services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+			services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("MyLocalConnection")));
 			services.AddControllersWithViews();
 			services.AddRazorPages();
 		}
