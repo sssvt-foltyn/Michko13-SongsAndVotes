d82db77c286bd90ad49cbd57daa38c8557a9c267
Pavel Foltyn <pavel.foltyn@volny.cz>
2021-04-16 11:08:39 +0200

    CHANGE Rename column UserUploadedID to UserID in the table Songs. Version "change4".

--- SongsAndVotesData/Tables/Songs.table.sql
+++ SongsAndVotesData/Tables/Songs.table.sql
@@ -3,15 +3,15 @@ CREATE TABLE Songs
     ID                 int               NOT NULL IDENTITY,
     Title              nvarchar(max)     NOT NULL,
     Photo              nvarchar(max)         NULL,
-    UserUploadedID     int                   NULL,
+    UserID             int                   NULL,
     PlaylistID         int                   NULL,
     AudioFile          nvarchar(max)         NULL,
     ArtistID           int                   NULL,
 
     CONSTRAINT PK_Songs  PRIMARY KEY (ID),
-    CONSTRAINT FK_Songs_Users_UserUploadedID   FOREIGN KEY (UserUploadedID)   REFERENCES Users (ID)      ON DELETE NO ACTION,
     CONSTRAINT FK_Songs_Artists_ArtistID       FOREIGN KEY (ArtistID)         REFERENCES Artists (ID)    ON DELETE NO ACTION,
-    CONSTRAINT FK_Songs_Playlists_PlaylistID   FOREIGN KEY (PlaylistID)       REFERENCES Playlists (ID)  ON DELETE NO ACTION
+    CONSTRAINT FK_Songs_Playlists_PlaylistID   FOREIGN KEY (PlaylistID)       REFERENCES Playlists (ID)  ON DELETE NO ACTION,
+    CONSTRAINT FK_Songs_Users_UserID           FOREIGN KEY (UserID)           REFERENCES Users (ID)      ON DELETE NO ACTION
 );
 GO
 
@@ -19,7 +19,7 @@ GO
 CREATE INDEX IX_Songs_PlaylistID ON Songs (PlaylistID);
 GO
 
-CREATE INDEX IX_Songs_UserUploadedID ON Songs (UserUploadedID);
+CREATE INDEX IX_Songs_UserID ON Songs (UserID);
 GO
 
 CREATE INDEX IX_Songs_ArtistID ON Songs (ArtistID);
